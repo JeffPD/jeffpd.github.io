725aff9b47e3d439afd9b5954b8d9976303e8a05
jeffd <jeffdatahan@gmail.com>
2021-04-02 16:06:21 -0600

    added contact

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         }
         
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if(this.scrollY > 600){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
@@ -22,7 +22,8 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+
+    $('.navbar').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
