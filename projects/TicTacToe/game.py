707cef43ca67d74d9a6658198e6fc6d06b3a4f36
jeffd <jeffdatahan@gmail.com>
2021-04-01 19:57:41 -0600

    moved projects folder
    accidently in website yikes

--- projects/TicTacToe/game.py
+++
@@ -1,7 +0,0 @@
-class TicTacToe:
-    def __init__(self):
-        self.board = [' ' for _ in range(9)] # 1D list for 3x3 board
-        self.current.winner = None # keep track of winner!
-
-        def print_board(self):
-            for row in [self.board[i*3:]]
